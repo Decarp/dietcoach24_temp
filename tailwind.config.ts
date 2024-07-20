9cba0fe16b7fc3d4e186f9baa7e17ae8404c5a98
Philipp John <philipp.john98@gmail.com>
2024-07-20 14:02:09 +0200

    add primary color, add tailwindcss forms

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#009900",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
 export default config;
