d4f3dbdfd7658d09330b1f13fd367dc541d98953
Philipp John <philipp.john98@gmail.com>
2024-07-20 15:44:27 +0200

    add basic layout, routing, and mock data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={inter.className + " h-full"}>{children}</body>
+      <body className={inter.className + " h-full"}>
+        <Header>
+          {children}
+        </Header>
+      </body>
     </html>
   );
 }
