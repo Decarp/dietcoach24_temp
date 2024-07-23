7108a220dbe1c217a80db5a5fcb5164e22b275f2
Philipp John <philipp.john98@gmail.com>
2024-07-23 13:48:09 +0200

    refactor charts

---
+++ utils/renderActiveChartShape.tsx
@@ -0,0 +1,43 @@
+import { Sector } from "recharts";
+
+const RADIAN = Math.PI / 180;
+
+export const renderActiveChartShape = (props: any) => {
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+
+  return (
+    <g>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+    </g>
+  );
+};
---
+++ utils/renderCustomizedChartLabel.tsx
@@ -0,0 +1,37 @@
+const RADIAN = Math.PI / 180;
+
+export const renderCustomizedChartLabel = (props: any) => {
+  const { cx, cy, midAngle, outerRadius, fill, payload, percent, value } =
+    props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
+
+  return (
+    <>
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#333"
+      >
+        {`${value} ${payload.metric} `}
+        <tspan fill="#999" fontSize="12" fontWeight="300">
+          ({(percent * 100).toFixed(0)}%)
+        </tspan>
+      </text>
+    </>
+  );
+};
