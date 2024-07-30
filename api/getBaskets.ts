94a9bfb133cf391825f958aad8b6d3f3676dae38
Philipp John <philipp.john98@gmail.com>
2024-07-30 11:20:19 +0200

    update api

--- api/getBaskets.ts
+++ api/getBaskets.ts
@@ -1,5 +1,18 @@
 import { basketsResponse } from "@/data/basketsResponse";
 
-export const getBaskets = () => {
+const fetchData = (
+  startTimestamp: number = 1000000, // default: 1970
+  endTimestamp: number = 10000000 // default: right now
+) => {
+  const authentication = ""; // via local storage
+  const participantId = ""; // via url param
+  return basketsResponse;
+};
+
+export const getBaskets = (
+  startTimestamp: number = 1000000, // default: 1970
+  endTimestamp: number = 10000000 // default: right now
+) => {
+  const data = fetchData(startTimestamp, endTimestamp);
   return basketsResponse;
 };
