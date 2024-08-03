5800e07f4059ea3c2f222ab258d8f5d37fb5af31
Philipp John <philipp.john98@gmail.com>
2024-08-03 17:41:47 +0200

    try fix deployment error

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -2,23 +2,19 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    const response = await fetch("http://localhost:8000/products/", {
-      method: "GET",
-      headers: {
-        retailer: "Migros",
-        Page: "1",
-        Limit: "110",
-        "Search-De": "Linsen",
-        "DietCoach-Category-L2-De": "Fleisch- & Fischalternativen",
-        "DietCoach-Category-L1-De": "Pflanzliche Proteinquellen",
-        "Nutri-Score-Cutoff": "C",
-      },
-    });
+  const response = await fetch("http://localhost:8000/products/", {
+    method: "GET",
+    headers: {
+      retailer: "Migros",
+      Page: "1",
+      Limit: "110",
+      "Search-De": "Linsen",
+      "DietCoach-Category-L2-De": "Fleisch- & Fischalternativen",
+      "DietCoach-Category-L1-De": "Pflanzliche Proteinquellen",
+      "Nutri-Score-Cutoff": "C",
+    },
+  });
 
-    const data = await response.json();
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.error();
-  }
+  const data = await response.json();
+  return NextResponse.json(data);
 }
