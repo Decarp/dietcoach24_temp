2fc8a6804641c666b6281cefca632e7f9746d130
Philipp John <philipp.john98@gmail.com>
2024-08-03 23:56:17 +0200

    test deploy route handlers (2)

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,29 @@
-import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  redirect("https://nextjs.org/");
+  return fetch("http://localhost:8000/products/", {
+    headers: {
+      retailer: "Migros",
+      Page: "1",
+      Limit: "110",
+      "Search-De": "Linsen",
+      "DietCoach-Category-L2-De": "Fleisch- & Fischalternativen",
+      "DietCoach-Category-L1-De": "Pflanzliche Proteinquellen",
+      "Nutri-Score-Cutoff": "C",
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        return NextResponse.json(
+          { error: `Error fetching data: ${res.statusText}` },
+          { status: res.status }
+        );
+      }
+      return res.json();
+    })
+    .then((data) => NextResponse.json(data))
+    .catch((error) => {
+      console.error("Fetch error:", error);
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    });
 }
