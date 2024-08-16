c531a0ac38c680d6e8177e8e2cc8faed9e8c0acb
Philipp John <philipp.john98@gmail.com>
2024-08-16 16:33:11 +0200

    update categories, sort categories a-z

--- components/purchases/recommendationDrawer/productPopup/FilterPopoverProduct.tsx
+++ components/purchases/recommendationDrawer/productPopup/FilterPopoverProduct.tsx
@@ -33,38 +33,46 @@ const FilterPopoverProduct = ({
         className="absolute left-0 h-96 overflow-y-scroll z-80 mt-2 origin-top-right rounded-md bg-white p-4 shadow-2xl ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
       >
         <form className="space-y-4">
-          {categoriesWithSub.map((category) => (
-            <div key={category.major}>
-              <div className="flex items-center">
-                <input
-                  value={category.major}
-                  type="checkbox"
-                  checked={selectedCategories.major.includes(category.major)}
-                  onChange={() => updateCategories(category.major, "major")}
-                  className="h-4 w-4 rounded border-gray-300 text-primary focus:ring-primary"
-                />
-                <label className="ml-3 whitespace-nowrap pr-6 text-sm font-medium text-gray-900">
-                  {category.major}
-                </label>
+          {categoriesWithSub
+            .sort((a, b) => {
+              if (a.major === "Ausgeschlossen") return 1; // Move "Ausgeschlossen" to the end
+              if (b.major === "Ausgeschlossen") return -1; // Move "Ausgeschlossen" to the end
+              return a.major.localeCompare(b.major);
+            })
+            .map((category) => (
+              <div key={category.major}>
+                <div className="flex items-center">
+                  <input
+                    value={category.major}
+                    type="checkbox"
+                    checked={selectedCategories.major.includes(category.major)}
+                    onChange={() => updateCategories(category.major, "major")}
+                    className="h-4 w-4 rounded border-gray-300 text-primary focus:ring-primary"
+                  />
+                  <label className="ml-3 whitespace-nowrap pr-6 text-sm font-medium text-gray-900">
+                    {category.major}
+                  </label>
+                </div>
+                <div className="ml-7">
+                  {category.subs
+                    .sort((a, b) => a.localeCompare(b))
+                    .map((subCategory) => (
+                      <div key={subCategory} className="flex items-center">
+                        <input
+                          value={subCategory}
+                          type="checkbox"
+                          checked={selectedCategories.sub.includes(subCategory)}
+                          onChange={() => updateCategories(subCategory, "sub")}
+                          className="h-4 w-4 rounded border-gray-300 text-primary focus:ring-primary"
+                        />
+                        <label className="ml-3 whitespace-nowrap pr-6 text-sm text-gray-900">
+                          {subCategory}
+                        </label>
+                      </div>
+                    ))}
+                </div>
               </div>
-              <div className="ml-7">
-                {category.subs.map((subCategory) => (
-                  <div key={subCategory} className="flex items-center">
-                    <input
-                      value={subCategory}
-                      type="checkbox"
-                      checked={selectedCategories.sub.includes(subCategory)}
-                      onChange={() => updateCategories(subCategory, "sub")}
-                      className="h-4 w-4 rounded border-gray-300 text-primary focus:ring-primary"
-                    />
-                    <label className="ml-3 whitespace-nowrap pr-6 text-sm text-gray-900">
-                      {subCategory}
-                    </label>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
+            ))}
         </form>
       </PopoverPanel>
     </Popover>
