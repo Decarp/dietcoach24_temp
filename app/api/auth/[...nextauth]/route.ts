f14bc42e25eb942227e7eaec02ff311d391065eb
Philipp John <philipp.john98@gmail.com>
2024-08-28 19:56:39 +0200

    clean up auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,6 @@
 import NextAuth, { User } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-async function getUser(token: string) {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/dietician/profile`,
-    {
-      method: "GET",
-      headers: {
-        Authentication: token,
-      },
-    }
-  );
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch user profile");
-  }
-
-  const user = await res.json();
-  return user;
-}
-
 const handler = NextAuth({
   providers: [
     CredentialsProvider({
@@ -33,7 +14,6 @@ const handler = NextAuth({
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        // Step 1: Log in the user and get the token
         const res = await fetch(
           `${process.env.NEXT_PUBLIC_API_URL}/user/login`,
           {
@@ -57,34 +37,29 @@ const handler = NextAuth({
           return null;
         }
 
-        // Step 3: Return a User object
         return {
-          id: loginData.email, // Using email as a unique identifier (ID)
+          id: loginData.email,
           email: loginData.email,
           token,
           type: loginData.type,
-        } as User; // Cast to User type
+        } as User;
       },
     }),
   ],
   callbacks: {
     async jwt({ token, user }) {
-      console.log("ROUTE jwt user:", user);
       if (user) {
-        const u = user as User; // Explicitly cast user to your User type
+        const u = user as User;
         token.accessToken = u.token;
         token.type = u.type;
       }
-      console.log("ROUTE jwt token:", token);
       return token;
     },
     async session({ session, token }) {
-      console.log("ROUTE token:", token);
       session.accessToken = token.accessToken;
       session.firstName = token.firstName;
       session.lastName = token.lastName;
       session.type = token.type;
-      console.log("ROUTE Session:", session);
       return session;
     },
   },
